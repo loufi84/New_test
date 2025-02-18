6baaeab634706d5a5cb9171160cbfe6f12c569e3
loufi84 <quentin.lataste@gmail.com>
2025-02-18 22:45:11 +0100

    Adding the division

---
+++ division.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * division - divide 2 int
+ * Description: This function will take 2 int
+ * and divide them
+ * @a: first int
+ * @b: second int
+ * Return: in
+ */
+
+int division(int a, int b)
+{
+	return (a / b);
+}
