63fce10098228362f4090f221f65d35cf3e8a124
loufi84 <quentin.lataste@gmail.com>
2025-02-18 22:47:13 +0100

    Adding the addition

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * addition - add 2 int
+ * Description: This function will take 2 int as
+ * parameters and return the sum
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int addition(int a, int b)
+{
+	return (a + b);
+}
