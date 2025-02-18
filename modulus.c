50cc6944fa982f10ef433c30ee1401504f410782
loufi84 <quentin.lataste@gmail.com>
2025-02-18 22:51:08 +0100

    Adding the modulus

---
+++ modulus.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * modulus - modulus 2 int
+ * Description: This function will take 2 int as parameters
+ * and return the result of a modulus
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int modulus(int a, int b)
+{
+	return (a % b);
+}
