36f121ab96d2e503d667bc82834a906e49a44171
loufi84 <quentin.lataste@gmail.com>
2025-02-18 22:49:04 +0100

    Adding the substraction possibility

---
+++ substraction.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * substraction - substract 2 int
+ * Description: This function will take 2 int
+ * as parameters and return the substract
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int substraction(int a, int b)
+{
+	return (a - b);
+}
